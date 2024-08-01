812ace5a4ab0e65031b32483a08e7207b97b8e07
Jagjeet <jagjeetsingh0424@gmail.com>
2024-08-02 00:01:04 +0530

    navigation menu created

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', () => {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = '#90AEAD';
+    } else {
+        navbar.style.backgroundColor = '#90AEAD';
+    }
+});
